b08a2809ecbb24c6d090adb531a87c4363582fa8
ekimaycicek <ekimaycicek@icloud.com>
2024-04-02 22:49:31 +0300

    new

--- src/main/java/LambdaExpression.java
+++ src/main/java/LambdaExpression.java
@@ -105,7 +105,6 @@ public class LambdaExpression {
 
 
 
-
     public static void FunctionalPrintEleman1(List<Integer> sayı ){// functional programing
 
         sayı.// aksiyona girecek list collections
